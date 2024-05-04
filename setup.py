47d00683377d11143ac3368113e4765a59ab126a
Mxytyu7 <130443471+Mxytyu7@users.noreply.github.com>
2024-05-04 22:38:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mxersion',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here
+    ],
+    author='Mxytyu',
+    author_email='mersion@gmail.com',
+    description='mxersion is an package to interact with mxersion's api / features.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/your_package_name',
+    license='MIT',
+)

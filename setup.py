a5feb0939f3caccb8b71707e9b8df9f06e8c8e77
Mxytyu7 <130443471+Mxytyu7@users.noreply.github.com>
2024-05-05 12:06:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     author='Mxytyu',
     author_email='mersion@gmail.com',
-    description='mxersion is an package to interact with mxersion s api and features.',
+    description='mxersion is an package to interact with mxersion s api and features',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/your_username/your_package_name',

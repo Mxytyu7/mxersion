0a2c7aac140631892370e9be6c22b432107dccb3
Mxytyu7 <130443471+Mxytyu7@users.noreply.github.com>
2024-09-16 12:53:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mxersion',
-    version='0.3',
+    version='0.01a',
     packages=find_packages(),
     install_requires=[
         # list your dependencies here

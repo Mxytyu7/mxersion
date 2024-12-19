6d5101d40c3d4986b548bc359a75b9a918fcc33e
Mxytyu7 <130443471+Mxytyu7@users.noreply.github.com>
2024-12-19 10:07:06 +0100

    Update hello.py

--- mxersion/hello.py
+++ mxersion/hello.py
@@ -1,4 +1,11 @@
 # hello.py
 
-def say_hello():
+def say_hello() -> None:
+    """
+    Prints a greeting message.
+    """
     print("Hello from mxersion!")
+
+
+if __name__ == "__main__":
+    say_hello()
